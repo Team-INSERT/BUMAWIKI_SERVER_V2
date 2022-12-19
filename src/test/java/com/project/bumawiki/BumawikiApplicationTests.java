e41d10fe1ab177617d98688ebc0e2350bffaeed5
jacobhboy <jacobhboy66@gmail.com>
2022-12-19 17:49:21 +0900

    init

---
+++ src/test/java/com/project/bumawiki/BumawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bumawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumawikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
