5255211a96f71077fdacdf1b3ff1c09c4db2fe3c
qlido <jason7530@naver.com>
2022-12-26 12:28:35 +0900

    Update: 이미지 처리 구현

--- src/main/java/com/project/bumawiki/domain/image/ImageController.java
+++ src/main/java/com/project/bumawiki/domain/image/ImageController.java
@@ -3,20 +3,22 @@ package com.project.bumawiki.domain.image;
 
 import com.project.bumawiki.domain.image.service.LoadImage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import com.project.bumawiki.domain.image.exception.NoImageException;
 
-import javax.annotation.Resource;
+import java.net.MalformedURLException;
 
 @Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class ImageController {
-    private final LoadImage Image;
+    private final LoadImage Image = new LoadImage();
     @GetMapping("/image/{fileName}")
-    public ResponseEntity<Resource> createDocs(@PathVariable("fileName") String fileName){
+    public ResponseEntity<UrlResource> GetImage(@PathVariable("fileName") String fileName) throws NoImageException, MalformedURLException {
         return Image.getImageByName(fileName);
     }
 
--- src/main/java/com/project/bumawiki/domain/image/service/LoadImage.java
+++ src/main/java/com/project/bumawiki/domain/image/service/LoadImage.java
@@ -1,34 +1,29 @@
 package com.project.bumawiki.domain.image.service;
 
-import org.springframework.core.io.FileSystemResource;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
+
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.annotation.Resource;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import com.project.bumawiki.domain.image.exception.NoImageException;
+import java.net.MalformedURLException;
 
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class LoadImage {
-    public ResponseEntity<Resource> getImageByName(@PathVariable("fileName") String fileName) {
-        ResponseEntity<Resource> resourceResponseEntity;
-        try {
-            String path = "실제 이미지가 있는 위치";
-            FileSystemResource resource = new FileSystemResource(path + fileName);
-            if (!resource.exists()) {
-                throw NoImageException.EXCEPTION;
-            }
-            HttpHeaders header = new HttpHeaders();
-            Path filePath;
-            filePath = Paths.get(path + fileName);
-            header.add("Content-Type", Files.probeContentType(filePath));
-            resourceResponseEntity = new ResponseEntity<Resource>((Resource) resource, header, HttpStatus.OK);
-            return resourceResponseEntity;
-        } catch (Exception e) {
-            throw NoImageException.EXCEPTION;
-        }
+
+    public ResponseEntity<UrlResource> getImageByName(@PathVariable("fileName") String fileName) throws MalformedURLException {
+        String imageRoot = "D:\\image\\";
+        String contentDisposition = "attachment; filename=\"" + fileName + "\"";
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .header("Content-type","image/"+fileName.split("\\.")[1])
+                .body(new UrlResource("file:" + imageRoot + fileName));
     }
 }
