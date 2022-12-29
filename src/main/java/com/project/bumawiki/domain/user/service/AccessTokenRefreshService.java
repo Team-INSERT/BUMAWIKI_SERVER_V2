596e57d3e75bf752d37932fe9633395a493b893e
jacobhboy <jacobhboy66@gmail.com>
2022-12-29 12:01:14 +0900

    fix : refresh토큰이 만료 되었을 때 404 refresh token not found 가 아닌 403 refresh token expired 에러 생성

--- src/main/java/com/project/bumawiki/domain/user/service/AccessTokenRefreshService.java
+++ src/main/java/com/project/bumawiki/domain/user/service/AccessTokenRefreshService.java
@@ -2,11 +2,9 @@ package com.project.bumawiki.domain.user.service;
 
 import com.project.bumawiki.domain.auth.domain.RefreshToken;
 import com.project.bumawiki.domain.auth.domain.repository.RefreshTokenRepository;
-import com.project.bumawiki.global.jwt.config.JwtProperties;
 import com.project.bumawiki.global.jwt.dto.TokenResponseDto;
-import com.project.bumawiki.global.jwt.exception.RefreshTokenNotFoundException;
+import com.project.bumawiki.global.jwt.exception.RefreshTokenExpiredException;
 import com.project.bumawiki.global.jwt.util.JwtProvider;
-import com.project.bumawiki.global.jwt.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +18,7 @@ public class AccessTokenRefreshService {
 
     public TokenResponseDto execute(String bearerRefreshToken) {
         RefreshToken redisRefreshToken = refreshTokenRepository.findByRefreshToken(bearerRefreshToken)
-                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+                .orElseThrow(() -> RefreshTokenExpiredException.EXCEPTION);
         return getNewAccessTokens(redisRefreshToken);
     }
 
