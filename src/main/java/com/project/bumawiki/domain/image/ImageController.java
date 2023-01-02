e29ba6353561318d70acd75a245760e91a663781
qlido <jason7530@naver.com>
2023-01-02 15:00:09 +0900

    FIX: 정리

--- src/main/java/com/project/bumawiki/domain/image/ImageController.java
+++ src/main/java/com/project/bumawiki/domain/image/ImageController.java
@@ -29,72 +29,24 @@ public class ImageController {
     public void FileController(StorageService storageService){
         this.storageService = storageService;
     }
-
-//    @PostMapping("/upload")
-//    public ResponseEntity<Response> uploadImage(@RequestParam("file") MultipartFile file,
-//                                                @RequestParam("DocsName") String DocsName,
-//                                                @RequestBody) throws IOException {
-//        Response res = new Response();
-//        try{
-//            String result = storageService.saveFile(file, DocsName);
-//            res.setImageLocation("/"+ DocsName +"/"+result);
-//            res.setMessage("done");
-//            res.setSuccess(true);
-//            return new ResponseEntity<Response>(res, HttpStatus.OK);
-//        }catch (Exception e){
-//            res.setMessage("failed");
-//            res.setSuccess(false);
-//            return new ResponseEntity<Response>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-////
-//    @PostMapping("/post/upload")
-//    public ResponseEntity<Response> postImageUpload(@RequestParam("files") MultipartFile[] files,
-//                                                    @RequestParam("postName")String postName,
-//                                                    @RequestBody("imageName") String[] ImageName) {
-//        Response res = new Response();
-//        List<String> results = new ArrayList<>();
-//        List<String> imageLocations = new ArrayList<>();
-//        try{
-//            results = storageService.saveFiles(files, postName, ImageName);
-//            for(String result : results){
-//                imageLocations.add("/"+postName+"/"+result);
-//            }
-//            res.setImageLocations(imageLocations);
-//            res.setMessage("done");
-//            res.setSuccess(true);
-//            return new ResponseEntity<Response>(res, HttpStatus.OK);
-//        }catch (Exception e){
-//            res.setMessage("failed");
-//            res.setSuccess(false);
-//            return new ResponseEntity<Response>(res, HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
-
     @GetMapping("/display/{DocsName}/{fileName}")
     public ResponseEntity<Resource> displayImage(@PathVariable String fileName,
                                                  @PathVariable String DocsName,
                                                  HttpServletRequest request) {
         // Load file as Resource
         Resource resource = storageService.loadFileAsResource(DocsName, fileName);
-
         // Try to determine file's content type
         String contentType = null;
         try {
             contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
             System.out.println(contentType);
         } catch (IOException ex) {
-            //logger.info("Could not determine file type.");
         }
-
-        // Fallback to the default content type if type could not be determined
         if(contentType == null) {
             contentType = "application/octet-stream";
         }
-
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .header("Content-Type",contentType)
                 .body(resource);
     }
