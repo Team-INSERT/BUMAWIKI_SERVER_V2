5f32e1b2a708417e2760dccd18b0d3cb924d592d
be <jacobhboy66@gmail.com>
2023-01-26 00:45:47 +0900

    add : setting for jenkins

--- src/test/java/com/project/bumawiki/BumawikiApplicationTests.java
+++ src/test/java/com/project/bumawiki/BumawikiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.project.bumawiki;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootTest
 class BumawikiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
