80a6bba92bbb220ad32749c05d38baa14df3989b
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-04-23 22:34:30 +0900

    Update(DocsService): 필요없는 imports 삭제

--- src/main/java/com/project/bumawiki/domain/docs/service/DocsPopularInformationService.java
+++ src/main/java/com/project/bumawiki/domain/docs/service/DocsPopularInformationService.java
@@ -3,9 +3,6 @@ package com.project.bumawiki.domain.docs.service;
 import com.project.bumawiki.domain.docs.domain.repository.DocsRepository;
 import com.project.bumawiki.domain.docs.presentation.dto.response.DocsPopularResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
