719066d638f402d9664f2e733ee92dff3d074ee5
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-06-12 10:58:18 +0900

    [#51]Refactor: Auth 리턴에서 ResponseEntity적용 및 refreshToken을 body가 아닌 header에서 가져옴

--- src/main/java/com/project/bumawiki/domain/auth/presentation/AuthController.java
+++ src/main/java/com/project/bumawiki/domain/auth/presentation/AuthController.java
@@ -1,20 +1,14 @@
 package com.project.bumawiki.domain.auth.presentation;
 
-import com.project.bumawiki.domain.auth.presentation.dto.RefreshTokenRequestBodyDto;
 import com.project.bumawiki.domain.auth.service.AccessTokenRefreshService;
 import com.project.bumawiki.domain.user.service.UserLoginService;
 import com.project.bumawiki.domain.user.service.UserLogoutService;
 import com.project.bumawiki.global.jwt.dto.TokenResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Validated
@@ -27,17 +21,17 @@ public class AuthController {
     private final AccessTokenRefreshService accessTokenRefreshService;
 
     @PostMapping("/oauth/bsm")
-    public TokenResponseDto userSignup(HttpServletRequest request) throws IOException {
-        return userLoginService.execute(request.getHeader("authCode"));
+    public TokenResponseDto userSignup(@RequestHeader("authCode") String authCode) throws IOException {
+        return ResponseEntity.ok(userLoginService.execute(authCode)).getBody();
     }
 
     @DeleteMapping("/bsm/logout")
-    public String userLogout(@RequestBody RefreshTokenRequestBodyDto request) {
-        return userLogoutService.execute(request.getRefresh_token());
+    public ResponseEntity<String> userLogout(@RequestHeader("refresh_token") String refreshToken) {
+        return ResponseEntity.ok(userLogoutService.execute(refreshToken));
     }
 
     @PutMapping("/refresh/access")
-    public TokenResponseDto refreshAccessToken(@RequestBody RefreshTokenRequestBodyDto request){
-        return accessTokenRefreshService.execute(request.getRefresh_token());
+    public TokenResponseDto refreshAccessToken(@RequestHeader("refresh_token") String refreshToken) {
+        return ResponseEntity.ok(accessTokenRefreshService.execute(refreshToken)).getBody();
     }
 }
--- src/main/java/com/project/bumawiki/domain/auth/presentation/dto/RefreshTokenRequestBodyDto.java
+++
@@ -1,8 +0,0 @@
-package com.project.bumawiki.domain.auth.presentation.dto;
-
-import lombok.Getter;
-
-@Getter
-public class RefreshTokenRequestBodyDto {
-    private String refresh_token;
-}
