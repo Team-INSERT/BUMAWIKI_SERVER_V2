4281f81a9bc7c0323c41c4ccf06958edb962e282
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-06-12 14:03:09 +0900

    [#51]Refactor: ThumbsUp Controller 리턴에 ResponseEntity 적용

--- src/main/java/com/project/bumawiki/domain/thumbsUp/presentation/ThumbsUpManipulateController.java
+++ src/main/java/com/project/bumawiki/domain/thumbsUp/presentation/ThumbsUpManipulateController.java
@@ -3,6 +3,7 @@ package com.project.bumawiki.domain.thumbsUp.presentation;
 import com.project.bumawiki.domain.thumbsUp.presentation.dto.ThumbsUpRequestDto;
 import com.project.bumawiki.domain.thumbsUp.service.ThumbsUpManipulateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,16 @@ public class ThumbsUpManipulateController {
     private final ThumbsUpManipulateService likesService;
 
     @PostMapping("/create")
-    public void createLike(@RequestBody ThumbsUpRequestDto likeRequestDto) {
+    public ResponseEntity<Object> createLike(@RequestBody ThumbsUpRequestDto likeRequestDto) {
         likesService.createDocsThumbsUp(likeRequestDto);
+
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete")
-    public void removeLike(@RequestBody ThumbsUpRequestDto likeRequestDto) {
+    public ResponseEntity<Object> removeLike(@RequestBody ThumbsUpRequestDto likeRequestDto) {
         likesService.removeLike(likeRequestDto);
+
+        return ResponseEntity.ok().build();
     }
 }
