edfe7529761ec70dc2944ca7c016a830b39b0dc3
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-06-15 23:51:48 +0900

    [#56]Fix: Random을 SecureRandom으로 변경

--- src/main/java/com/project/bumawiki/domain/image/service/ImageService.java
+++ src/main/java/com/project/bumawiki/domain/image/service/ImageService.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -25,7 +26,7 @@ public class ImageService {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
-        Random random = new Random();
+        SecureRandom random = new SecureRandom();
         String generatedString = random.ints(leftLimit, rightLimit + 1)
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
