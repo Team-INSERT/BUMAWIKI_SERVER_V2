d0954ee5c8357b181228a2408441f11b3628477b
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-06-16 11:21:42 +0900

    [#56]Refactor: unused import 제거

--- src/main/java/com/project/bumawiki/domain/image/service/ImageService.java
+++ src/main/java/com/project/bumawiki/domain/image/service/ImageService.java
@@ -16,13 +16,12 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Random;
 
 @Service
 public class ImageService {
     private static final String uploadPath = "/home/insert/Desktop/image/";
 
-    private static String getRandomStr(){
+    private static String getRandomStr() {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
@@ -35,13 +34,13 @@ public class ImageService {
         return generatedString;
     }
 
-    public String saveFile(MultipartFile file, String userName) throws IOException{
+    public String saveFile(MultipartFile file, String userName) throws IOException {
 
         String randomStr = getRandomStr();
         String fileName = randomStr + StringUtils.cleanPath(file.getOriginalFilename());
 
-        Path uploadPath = Paths.get(ImageService.uploadPath+userName);
-        if(!Files.exists(uploadPath)) {
+        Path uploadPath = Paths.get(ImageService.uploadPath + userName);
+        if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
 
@@ -53,21 +52,22 @@ public class ImageService {
             throw new IOException("Could not save image file: " + fileName, ioe);
         }
     }
+
     public ArrayList<String> GetFileUrl(MultipartFile[] files, String DocsName) throws IOException {
         ArrayList<String> ImageUrl = new ArrayList<>();
-        for (MultipartFile file : files){
-            String fileName = saveFile(file,DocsName);
-            ImageUrl.add("<<https://bumawiki.kro.kr/api/image/display/"+DocsName+"/"+fileName+">>");
+        for (MultipartFile file : files) {
+            String fileName = saveFile(file, DocsName);
+            ImageUrl.add("<<https://bumawiki.kro.kr/api/image/display/" + DocsName + "/" + fileName + ">>");
         }
         return ImageUrl;
     }
 
     public Resource loadFileAsResource(String DocsName, String fileName) {
-        Path uploadPath = Paths.get(ImageService.uploadPath,DocsName);
+        Path uploadPath = Paths.get(ImageService.uploadPath, DocsName);
         try {
             Path filePath = uploadPath.resolve(fileName).normalize();
             Resource resource = new UrlResource(filePath.toUri());
-            if(resource.exists()) {
+            if (resource.exists()) {
                 return resource;
             } else {
                 throw NoImageException.EXCEPTION;
