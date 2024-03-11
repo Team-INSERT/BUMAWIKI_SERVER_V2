6c2713a40c30a31ae8965874c23611a094883fac
bo <jacobhboy66@gmail.com>
2024-03-11 15:47:45 +0900

    Refactor: 문서 type조회시 simple contents(문서 200자) 추가 전달

--- src/main/java/com/project/bumawiki/domain/docs/presentation/dto/response/DocsNameAndEnrollResponseDto.java
+++ src/main/java/com/project/bumawiki/domain/docs/presentation/dto/response/DocsNameAndEnrollResponseDto.java
@@ -1,10 +1,12 @@
 package com.project.bumawiki.domain.docs.presentation.dto.response;
 
 import com.project.bumawiki.domain.docs.domain.Docs;
+import com.project.bumawiki.domain.docs.domain.VersionDocs;
 import com.project.bumawiki.domain.docs.domain.type.DocsType;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class DocsNameAndEnrollResponseDto {
@@ -12,6 +14,7 @@ public class DocsNameAndEnrollResponseDto {
     private Long id;
     private String title;
     private int enroll;
+    private String simpleContents;
     private DocsType docsType;
     private LocalDateTime lastModifiedAt;
 
@@ -21,5 +24,14 @@ public class DocsNameAndEnrollResponseDto {
         this.enroll = docs.getEnroll();
         this.docsType = docs.getDocsType();
         this.lastModifiedAt = docs.getLastModifiedAt();
+        this.simpleContents = getSimpleContents(docs);
+    }
+
+    public String getSimpleContents(Docs docs) {
+        List<VersionDocs> docsVersion = docs.getDocsVersion();
+        int currentDocsSize = docsVersion.size() - 1;
+        String contents = docsVersion.get(currentDocsSize).getContents();
+        int endIndex = Math.min(contents.length(), 200);
+        return contents.substring(0, endIndex);
     }
 }
