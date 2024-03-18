2757984e01d4bd0243b535b1867fa59b188f8f45
qlido <jason7530@naver.com>
2024-03-18 12:20:55 +0900

    Debug: print what docs type is not found

--- src/main/java/com/project/bumawiki/domain/docs/presentation/DocsInformationController.java
+++ src/main/java/com/project/bumawiki/domain/docs/presentation/DocsInformationController.java
@@ -48,7 +48,8 @@ public class DocsInformationController {
 
 		DocsType docsType = DocsType.valueOfLabel(stringDocsType);
 		if (docsType == null) {
-			System.out.println(stringDocsType);
+			System.out.println();
+			System.out.println("Not found Docs type : " + stringDocsType);
 			throw DocsTypeNotFoundException.EXCEPTION;
 		}
 
